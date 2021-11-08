0b725ed9deaa9e0460a5cf1562ba168c7acaa157
Miguel <miguelangel53013@gmail.com>
2021-11-07 19:47:59 -0500

    committing update to window size

--- src/main/java/baseline/ToDoListApplication.java
+++ src/main/java/baseline/ToDoListApplication.java
@@ -19,7 +19,7 @@ public class ToDoListApplication extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("MainMenuScene.fxml")));
-        Scene scene = new Scene(root, 1158, 658);
+        Scene scene = new Scene(root, 1158, 667);
         stage.setTitle("To Do List Application");
         stage.setScene(scene);
         stage.show();
